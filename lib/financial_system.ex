f03299e0e9d6a273cedd55f65d990c36b42ddee9
Cristine Guadelupe <cristineguadelupe@me.com>
2020-12-28 02:27:25 -0300

    Feat: Cria a operação de transferência entre duas contas e entre a mesma moeda

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -1,18 +1,63 @@
 defmodule FinancialSystem do
   @moduledoc """
-  Documentation for `FinancialSystem`.
+  Implementa as operações de tranferência entre duas ou mais contas
   """
 
-  @doc """
-  Hello world.
+  alias FinancialSystem.Schemas.Account
+  alias FinancialSystem.Operations
 
-  ## Examples
-
-      iex> FinancialSystem.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @spec transfer_from_to(sender :: Account.t(), receiver :: Account.t(), amount :: String.t()) ::
+          {atom(), {Account.t(), Account.t()}} | {atom(), String.t()}
+  def transfer_from_to(sender, receiver, amount) when is_binary(amount) do
+    transfer_from_to(sender, receiver, to_int(amount))
   end
+
+  @spec transfer_from_to(sender :: Account.t(), receiver :: Account.t(), amount :: integer()) ::
+          {atom(), {Account.t(), Account.t()}} | {atom(), String.t()}
+  def transfer_from_to(sender, receiver, amount) do
+    with {:ok, sender} <- debit_from(sender, amount),
+         {:ok, receiver} <- deposit_to(receiver, amount) do
+      {:ok, {sender, receiver}}
+    end
+  end
+
+  defp debit_from({:ok, account}, amount) do
+    account
+    |> Map.fetch!(:balance)
+    |> Operations.withdraw(amount)
+    |> maybe_update_account(account)
+  end
+
+  defp debit_from({:error, reason}, _amount), do: {:error, reason}
+  defp debit_from(_account, _amount), do: {:error, "Operação inválida"}
+
+  defp deposit_to({:ok, account}, amount) do
+    account
+    |> Map.fetch!(:balance)
+    |> Operations.deposit(amount)
+    |> maybe_update_account(account)
+  end
+
+  defp deposit_to({:error, reason}, _amount), do: {:error, reason}
+  defp deposit_to(_account, _amount), do: {:error, "Operação inválida"}
+
+  defp maybe_update_account({:ok, balance}, account) do
+    {:ok, %Account{account | balance: balance}}
+  end
+
+  defp maybe_update_account(error, _account), do: error
+
+  defp to_int(amount) do
+    amount
+    |> String.match?(~r/^[\d_,\.]+$/)
+    |> validate_amount(amount)
+  end
+
+  defp validate_amount(true, amount) do
+    amount
+    |> String.replace(~r/\D/, "")
+    |> String.to_integer()
+  end
+
+  defp validate_amount(false, _amount), do: :error
 end
--- test/financial_system_test.exs
+++ test/financial_system_test.exs
@@ -1,8 +1,157 @@
 defmodule FinancialSystemTest do
   use ExUnit.Case
-  doctest FinancialSystem
 
-  test "greets the world" do
-    assert FinancialSystem.hello() == :world
+  alias FinancialSystem.Schemas.Account
+  alias FinancialSystem.Schemas.Currency
+  alias FinancialSystem.Schemas.Money
+
+  alias FinancialSystem.Data.Accounts
+
+  @sender Accounts.open_account(1, "250,00")
+  @receiver Accounts.open_account(2, "10,00")
+  @not_found Accounts.find(5)
+
+  describe "transfer_from_to/3" do
+    test "Retorna os saldos atualizados após uma transferência válida" do
+      sender_after_send_100_00 = %Account{
+        balance: %Money{
+          currency: %Currency{
+            code: "BRL",
+            name: "Real",
+            number: 986,
+            precision: 2
+          },
+          decimal: 0,
+          int: 150
+        },
+        id: 1
+      }
+
+      receiver_after_receive_100_00 = %Account{
+        balance: %Money{
+          currency: %Currency{
+            code: "BRL",
+            name: "Real",
+            number: 986,
+            precision: 2
+          },
+          decimal: 0,
+          int: 110
+        },
+        id: 2
+      }
+
+      sender_after_send_250_00 = %Account{
+        balance: %Money{
+          currency: %Currency{
+            code: "BRL",
+            name: "Real",
+            number: 986,
+            precision: 2
+          },
+          decimal: 0,
+          int: 0
+        },
+        id: 1
+      }
+
+      receiver_after_receive_250_00 = %Account{
+        balance: %Money{
+          currency: %Currency{
+            code: "BRL",
+            name: "Real",
+            number: 986,
+            precision: 2
+          },
+          decimal: 0,
+          int: 260
+        },
+        id: 2
+      }
+
+      sender_after_send_95_12 = %Account{
+        balance: %Money{
+          currency: %Currency{
+            code: "BRL",
+            name: "Real",
+            number: 986,
+            precision: 2
+          },
+          decimal: 88,
+          int: 154
+        },
+        id: 1
+      }
+
+      receiver_after_receive_95_12 = %Account{
+        balance: %Money{
+          currency: %Currency{
+            code: "BRL",
+            name: "Real",
+            number: 986,
+            precision: 2
+          },
+          decimal: 12,
+          int: 105
+        },
+        id: 2
+      }
+
+      assert FinancialSystem.transfer_from_to(@sender, @receiver, 100_00) ==
+               {:ok, {sender_after_send_100_00, receiver_after_receive_100_00}}
+
+      assert FinancialSystem.transfer_from_to(@sender, @receiver, 250_00) ==
+               {:ok, {sender_after_send_250_00, receiver_after_receive_250_00}}
+
+      assert FinancialSystem.transfer_from_to(@sender, @receiver, 95_12) ==
+               {:ok, {sender_after_send_95_12, receiver_after_receive_95_12}}
+
+      assert FinancialSystem.transfer_from_to(@sender, @receiver, "95,12") ==
+               {:ok, {sender_after_send_95_12, receiver_after_receive_95_12}}
+    end
+
+    test "Falha ao tentar transferir valores maiores que o saldo disponível" do
+      assert FinancialSystem.transfer_from_to(@sender, @receiver, 350_00) ==
+               {:error, "Saldo insuficiente"}
+
+      assert FinancialSystem.transfer_from_to(@sender, @receiver, 250_01) ==
+               {:error, "Saldo insuficiente"}
+
+      assert FinancialSystem.transfer_from_to(@sender, @receiver, "2.050,01") ==
+               {:error, "Saldo insuficiente"}
+    end
+
+    test "Falha ao tentar transferir valores negativos ou inválidos" do
+      assert FinancialSystem.transfer_from_to(@sender, @receiver, -123_09) ==
+               {:error, "O valor a ser debitado precisa ser positivo"}
+
+      assert FinancialSystem.transfer_from_to(@sender, @receiver, "-123_09") ==
+               {:error, "O valor a ser debitado precisa ser positivo"}
+
+      assert FinancialSystem.transfer_from_to(@sender, @receiver, 0) ==
+               {:error, "O valor a ser debitado precisa ser positivo"}
+
+      assert FinancialSystem.transfer_from_to(@sender, @receiver, "Inválido") ==
+               {:error, "O valor a ser debitado precisa ser positivo"}
+
+      assert FinancialSystem.transfer_from_to(@sender, @receiver, "10not12309a90,80number") ==
+               {:error, "O valor a ser debitado precisa ser positivo"}
+    end
+
+    test "Falha ao tentar transferir entre contas inválidas" do
+      assert FinancialSystem.transfer_from_to(@sender, "Inválida", 125_00) ==
+               {:error, "Operação inválida"}
+
+      assert FinancialSystem.transfer_from_to("Inválida", @receiver, 125_00) ==
+               {:error, "Operação inválida"}
+    end
+
+    test "Falha ao tentar transferir para uma conta não encontrada" do
+      assert FinancialSystem.transfer_from_to(@sender, @not_found, 125_00) ==
+               {:error, "Conta não encontrada"}
+
+      assert FinancialSystem.transfer_from_to(@not_found, @receiver, 125_00) ==
+               {:error, "Conta não encontrada"}
+    end
   end
 end
