bab684af72cf812c5e7530fdbc9bfa7c76871037
Cristine Guadelupe <cristineguadelupe@me.com>
2020-12-29 02:05:36 -0300

    Refactor: Moneys_test - adequando os testes cujo o assert sejam pattern match ao guia de estilo do Elixir

--- test/moneys_test.exs
+++ test/moneys_test.exs
@@ -8,9 +8,9 @@ defmodule FinancialSystem.MoneysTest do
     test "Retorna o valor monetário" do
       real = Currencies.find("BRL") |> elem(1)
       euro = Currencies.find("EUR") |> elem(1)
-      assert Moneys.create(10, 00, real) == {:ok, %Money{int: 10, decimal: 00, currency: real}}
-      assert Moneys.create(25, 50, real) == {:ok, %Money{int: 25, decimal: 50, currency: real}}
-      assert Moneys.create(57, 03, euro) == {:ok, %Money{int: 57, decimal: 03, currency: euro}}
+      assert {:ok, %Money{int: 10, decimal: 00, currency: real}} == Moneys.create(10, 00, real)
+      assert {:ok, %Money{int: 25, decimal: 50, currency: real}} == Moneys.create(25, 50, real)
+      assert {:ok, %Money{int: 57, decimal: 03, currency: euro}} == Moneys.create(57, 03, euro)
     end
   end
 end
