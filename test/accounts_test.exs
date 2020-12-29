f6ec9d60ce7d5ffd566d0ae47312e184e73c18d9
Cristine Guadelupe <cristineguadelupe@me.com>
2020-12-29 02:00:23 -0300

    Refactor: Accounts_test - adequando os testes cujo o assert sejam pattern match ao guia de estilo do Elixir

--- test/accounts_test.exs
+++ test/accounts_test.exs
@@ -10,13 +10,13 @@ defmodule FinancialSystem.AccountsTest do
   describe "open_account/2" do
     test "Retorna a conta aberta" do
       account_1 = %Account{id: 1, balance: Moneys.create(25230, 00, @real) |> elem(1)}
-      assert Accounts.open_account(1, "25.230,00") == {:ok, account_1}
+      assert {:ok, account_1} == Accounts.open_account(1, "25.230,00")
 
       account_2 = %Account{id: 2, balance: Moneys.create(525, 12, @real) |> elem(1)}
-      assert Accounts.open_account(2, "525,12") == {:ok, account_2}
+      assert {:ok, account_2} == Accounts.open_account(2, "525,12")
 
       account_3 = %Account{id: 3, balance: Moneys.create(0, 00, @real) |> elem(1)}
-      assert Accounts.open_account(3, "0,00") == {:ok, account_3}
+      assert {:ok, account_3} == Accounts.open_account(3, "0,00")
     end
   end
 
@@ -24,7 +24,7 @@ defmodule FinancialSystem.AccountsTest do
     test "Retorna a conta criada" do
       account_1 = %Account{id: 1, balance: Moneys.create(100, 00, @real) |> elem(1)}
       balance_1 = Moneys.create(100, 00, @real) |> elem(1)
-      assert Accounts.create(1, balance_1) == {:ok, account_1}
+      assert {:ok, account_1} == Accounts.create(1, balance_1)
     end
   end
 
@@ -34,21 +34,21 @@ defmodule FinancialSystem.AccountsTest do
       account_2 = %Account{id: 2, balance: Moneys.create(5000, 25, @real) |> elem(1)}
       account_3 = %Account{id: 3, balance: Moneys.create(225, 10, @real) |> elem(1)}
 
-      assert Accounts.find(1) == {:ok, account_1}
-      assert Accounts.find(2) == {:ok, account_2}
-      assert Accounts.find(3) == {:ok, account_3}
+      assert {:ok, account_1} == Accounts.find(1)
+      assert {:ok, account_2} == Accounts.find(2)
+      assert {:ok, account_3} == Accounts.find(3)
     end
 
     test "Falha ao procurar por uma conta não cadastrada" do
-      assert Accounts.find(4) == {:error, "Conta não encontrada"}
-      assert Accounts.find(5) == {:error, "Conta não encontrada"}
-      assert Accounts.find(123) == {:error, "Conta não encontrada"}
+      assert {:error, "Conta não encontrada"} == Accounts.find(4)
+      assert {:error, "Conta não encontrada"} == Accounts.find(5)
+      assert {:error, "Conta não encontrada"} == Accounts.find(123)
     end
 
     test "Falha ao procurar por um conta utilizando um identificador inválido" do
-      assert Accounts.find("1") == {:error, "Conta inválida"}
-      assert Accounts.find("conta") == {:error, "Conta inválida"}
-      assert Accounts.find([1, 2, 3]) == {:error, "Conta inválida"}
+      assert {:error, "Conta inválida"} == Accounts.find("1")
+      assert {:error, "Conta inválida"} == Accounts.find("conta")
+      assert {:error, "Conta inválida"} == Accounts.find([1, 2, 3])
     end
   end
 end
