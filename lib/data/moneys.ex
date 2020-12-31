1a0ebbc543b6101c0c485c386cf8af270fd27b60
Cristine Guadelupe <cristineguadelupe@me.com>
2020-12-31 04:37:55 -0300

    Feat: Adiciona validação na criação de valores monetários

--- lib/data/moneys.ex
+++ lib/data/moneys.ex
@@ -9,14 +9,31 @@ defmodule FinancialSystem.Data.Moneys do
   @spec create(int :: integer(), decimal :: integer(), currency :: Currency.t()) ::
           {:ok, Money.t()} | {:error, String.t()}
   def create(int, decimal, currency) do
-    money = %Money{int: int, decimal: decimal, currency: currency}
-    {:ok, money}
+    with {:ok, int} <- validate_int(int),
+         {:ok, decimal} <- validate_decimal(decimal, currency) do
+      money = %Money{int: int, decimal: decimal, currency: currency}
+      {:ok, money}
+    end
   end
 
-  # TODO: validação dos valores
-  # currency precisa ser uma moeda válida e disponível
-  # int precisa ser inteiro
-  # decimal precisa ser inteiro e corresponder a precisão da moeda
-  # o valor precisa ter os 3 campos válidos
-  # ou retornar valor inválido e o motivo
+  defp validate_int(int) when is_integer(int) and int >= 0, do: {:ok, int}
+  defp validate_int(_int), do: {:error, "A parte inteira não pode ser negativa"}
+
+  defp validate_decimal(decimal, _currency) when is_integer(decimal) and decimal < 0 do
+    {:error, "A parte decimal não pode ser negativa"}
+  end
+
+  defp validate_decimal(0 = decimal, _currency), do: {:ok, decimal}
+
+  defp validate_decimal(decimal, %Currency{precision: precision}) when is_integer(decimal) do
+    decimal_precision =
+      decimal
+      |> Integer.digits()
+      |> length()
+
+    case decimal_precision do
+      decimal_precision when decimal_precision <= precision -> {:ok, decimal}
+      _ -> {:error, "A parte decimal não pode conter mais dígitos que a precisão da moeda"}
+    end
+  end
 end
--- test/moneys_test.exs
+++ test/moneys_test.exs
@@ -1,16 +1,50 @@
 defmodule FinancialSystem.MoneysTest do
   use ExUnit.Case
   alias FinancialSystem.Schemas.Money
+  alias FinancialSystem.Schemas.Currency
   alias FinancialSystem.Data.Moneys
   alias FinancialSystem.Data.Currencies
 
   describe "create/3" do
-    test "Retorna o valor monetário" do
-      real = Currencies.find("BRL") |> elem(1)
-      euro = Currencies.find("EUR") |> elem(1)
-      assert {:ok, %Money{int: 10, decimal: 00, currency: real}} == Moneys.create(10, 00, real)
-      assert {:ok, %Money{int: 25, decimal: 50, currency: real}} == Moneys.create(25, 50, real)
-      assert {:ok, %Money{int: 57, decimal: 03, currency: euro}} == Moneys.create(57, 03, euro)
+    setup do
+      [
+        real: Currencies.find("BRL") |> elem(1),
+        euro: Currencies.find("EUR") |> elem(1)
+      ]
+    end
+
+    test "Retorna o valor monetário", test_data do
+      assert {:ok,
+              %Money{
+                int: 10,
+                decimal: 00,
+                currency: %Currency{code: "BRL", name: "Real", number: 986, precision: 2}
+              }} = Moneys.create(10, 00, test_data[:real])
+
+      assert {:ok,
+              %Money{
+                int: 25,
+                decimal: 50,
+                currency: %Currency{code: "BRL", name: "Real", number: 986, precision: 2}
+              }} = Moneys.create(25, 50, test_data[:real])
+
+      assert {:ok,
+              %Money{
+                int: 57,
+                decimal: 03,
+                currency: %Currency{code: "EUR", name: "Euro", number: 978, precision: 2}
+              }} = Moneys.create(57, 03, test_data[:euro])
+    end
+
+    test "Falha ao informar valores inválidos", test_data do
+      assert {:error, "A parte decimal não pode ser negativa"} =
+               Moneys.create(123, -8, test_data[:real])
+
+      assert {:error, "A parte inteira não pode ser negativa"} =
+               Moneys.create(-98, 25, test_data[:real])
+
+      assert {:error, "A parte decimal não pode conter mais dígitos que a precisão da moeda"} =
+               Moneys.create(123, 128, test_data[:real])
     end
   end
 end
